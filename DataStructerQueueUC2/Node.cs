26bc4dc4cbd4e6f06bd51c75d18fd82456c3682a
Sangmesh Methre <SangmeshMetre>
2022-11-07 19:40:14 +0530

    DeQueueProgram

---
+++ DataStructerQueueUC2/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructerQueueUC2
+{
+    public class Node
+    {
+
+        //define variables
+        public int data;
+        public Node Next;
+
+        //parameterized constructor
+        public Node(int data)
+        {
+            this.data = data;
+            Next = null;
+        }
+    }
+}
