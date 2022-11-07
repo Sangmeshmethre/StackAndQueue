ba2640d013390bc5e4e299813285e4f5d2cadfc9
Sangmesh Methre <SangmeshMetre>
2022-11-07 19:27:03 +0530

    StackLinkedList

---
+++ DataStructureStackUC2/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureStackUC2
+{
+    public class Node
+    {
+        public int data;
+        public Node Next;
+
+        //constructor of Node class
+        public Node(int data)
+        {
+            this.data = data;
+            Next = null;
+        }
+    }
+}
