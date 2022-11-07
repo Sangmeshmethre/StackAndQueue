5511addc576ad212364b5edd1cf8ed2775d7c251
Sangmesh Methre <SangmeshMetre>
2022-11-07 19:18:04 +0530

    StackLinkedList

---
+++ DataStructureStackUC1/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureStackUC1
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        //constructor of Node class
+        public Node(int data)
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+}
---
+++ DataStructureStackUC1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureStackUC1
+{
+    public class Progarm
+    {
+        public static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+            linkedListStack.Display();
+        }
+    }
+}
